e2241117ec19ae24364307f579c81a608c508edb
Lnvictor <victorhpereira26@gmail.com>
2023-05-03 08:59:43 -0300

    study

---
+++ src/animal_factory/animal_factory.rb
@@ -0,0 +1,11 @@
+class Animal
+    def initialize(name, age, species)
+        @name = name
+        @age = age
+        @species
+    end
+end
+
+
+animal = Animal.new("Simba", 12, "lion")
+print animal.is_a? Animal
